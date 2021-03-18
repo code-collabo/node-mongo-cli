bad06591e3d159545b5da89aa012f9132fb2311e
ifycode <marychord@gmail.com>
2021-03-18 15:41:28 -0800

    empty folders deleted, restore failing test

--- spec/cli.spec.js
+++ spec/cli.spec.js
@@ -1,10 +1,9 @@
-//import { cli as cli } from '../src/cli';
+import { cli as cli } from '../src/cli';
 
 describe('Initial test: cli function', () => {
 
   it('should return undefined', () => {
-    //let test = cli();
-    //expect(test).toBeUndefined();
-    expect(true).toBe(true);
+    let test = cli();
+    expect(test).toBeUndefined();
   });
 });
