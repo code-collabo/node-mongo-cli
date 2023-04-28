e460f593f3227ef1af1c120e3f72ca4666274048
Dkingofcode <doladepo128@gmail.com>
2023-04-28 17:40:02 +0100

    added extra error messages

--- templates/esm/src/server.js
+++ templates/esm/src/server.js
@@ -12,5 +12,7 @@ server.listen(port, () => {
     console.log( chalk.cyanBright(`\nServer running at http://localhost:${port}`) );
   }catch(err) {
     console.log(err);
+    console.log(chalk.redBright(`\nYou can report the error you encountered at https://github.com/code-collabo/node-mongo-cli/issues/new?assignees=&labels=bug&template=cli-user-error-report.md\n`));
+    console.log(chalk.redBright(`\nThere is a manual download option for the API boilerplate templates at: https://github.com/code-collabo/node-mongo-api-boilerplate-templates\n`))
   }
 });
