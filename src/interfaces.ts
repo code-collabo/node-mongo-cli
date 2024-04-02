b313dcb6c31a1668ef28b346d051f6858fc9dd2f
Ifycode <marychord@gmail.com>
2024-04-02 22:04:06 +0100

    clean up formatting

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,25 +1,25 @@
 export interface TemplateQuestions {
-    type: string;
-    name: string;
-    message: string;
-    choices: string[];
-    default: string;
-  }
+  type: string;
+  name: string;
+  message: string;
+  choices: string[];
+  default: string;
+}
 
 export type ItemplateOptions = string;
 
 export interface Ioptions {
-    skipPrompts: boolean;
-    git: boolean;
-    skipGit: string | undefined;
-    folderName: string;
-    template: ItemplateOptions;
-    targetDirectory: string;
-    templateDirectory: string;
-    runInstall: boolean;
-    skipInstall: boolean;
-    help: string | boolean;
-    version: string | boolean;
+  skipPrompts: boolean;
+  git: boolean;
+  skipGit: string | undefined;
+  folderName: string;
+  template: ItemplateOptions;
+  targetDirectory: string;
+  templateDirectory: string;
+  runInstall: boolean;
+  skipInstall: boolean;
+  help: string | boolean;
+  version: string | boolean;
 }
 
 export interface IFoldernameAnswers {
