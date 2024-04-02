8a0e6300c1a08d6032eee090f21ced4b1dec1837
Ifycode <marychord@gmail.com>
2024-04-02 22:28:00 +0100

    clean up package.json scripts and start message

--- dev/customize/startMessage.js
+++ dev/customize/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk';/*eslint-disable no-console*/
 
-console.log( chalk.greenBright('\nnode-mongo-cli: running in dev mode...\n') );
+console.log( chalk.greenBright('\nℹ Collabo Community "Create Collabo App": running in dev mode...\n') );
